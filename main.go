a116c0c390665897aa4b0161fac79b8500518db8
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2019-05-08 17:23:43 +0700

    timeout is 5 secs

--- main.go
+++ main.go
@@ -58,7 +58,7 @@ func callDefault(c echo.Context) error {
 			DisableKeepAlives: true,
 			TLSClientConfig:   &tls.Config{InsecureSkipVerify: true},
 		},
-		Timeout: time.Second,
+		Timeout: time.Second * 5,
 	}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
